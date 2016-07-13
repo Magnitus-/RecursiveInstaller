284a1905415695ea26e4d62df42ce8fc04935b8b
Eric Vallee <eric_vallee@webificservices.com>
2016-07-13 03:19:34 -0400

    Adjustements

--- test/index.js
+++ test/index.js
@@ -7,12 +7,6 @@ const actOnModules = require('act-on-modules');
 
 exports.test = {
     'tearDown': function(callback) {
-        actOnModules.sync(path.join(__dirname, 'app'), (directory) => {
-            childProcess.execSync('rm -R node_modules', {'cwd': directory});
-        });
-        actOnModules.sync(path.join(__dirname, 'shared'), (directory) => {
-            childProcess.execSync('rm -R node_modules', {'cwd': directory});
-        });
         callback();
     },
     'basic_functionality': function(test) {
@@ -30,6 +24,76 @@ exports.test = {
             {
                 console.log(err);
             }
+        }).then(() => {
+            actOnModules.sync(path.join(__dirname, 'app'), (directory) => {
+                childProcess.execSync('rm -R node_modules', {'cwd': directory});
+            });
+            actOnModules.sync(path.join(__dirname, 'shared'), (directory) => {
+                childProcess.execSync('rm -R node_modules', {'cwd': directory});
+            });
+        }).catch((err) => {
+            console.log(err);
+        }).finally(() => {
+            test.done();
+        });
+    },
+    'explicitInstalls': function(test) {
+        test.expect(2);
+        recursiveInstaller([path.join(__dirname, 'explicitInstalls')], null, 1024 * 500).then(() => {
+            return Promise.all([recursiveInstaller([path.join(__dirname, 'explicitInstalls', 'prod')], '--production', 1024 * 500, true),
+                                recursiveInstaller([path.join(__dirname, 'explicitInstalls', 'dev')], null, 1024 * 500, true)]);
+        }).then(() => {
+            try
+            {
+                fs.accessSync(path.join(__dirname, 'explicitInstalls', 'prod', 'node_modules', 'hapi'), fs.F_OK);
+                fs.accessSync(path.join(__dirname, 'explicitInstalls', 'prod', 'node_modules', 'joi'), fs.F_OK);
+                fs.accessSync(path.join(__dirname, 'explicitInstalls', 'prod', 'node_modules', 'boom'), fs.F_OK);
+                fs.accessSync(path.join(__dirname, 'explicitInstalls', 'prod', 'node_modules', 'bluebird'), fs.F_OK);
+                fs.accessSync(path.join(__dirname, 'explicitInstalls', 'dev', 'node_modules', 'hapi'), fs.F_OK);
+                fs.accessSync(path.join(__dirname, 'explicitInstalls', 'dev', 'node_modules', 'joi'), fs.F_OK);
+                fs.accessSync(path.join(__dirname, 'explicitInstalls', 'dev', 'node_modules', 'boom'), fs.F_OK);
+                fs.accessSync(path.join(__dirname, 'explicitInstalls', 'dev', 'node_modules', 'bluebird'), fs.F_OK);
+                fs.accessSync(path.join(__dirname, 'explicitInstalls', 'dev', 'node_modules', 'nodeunit'), fs.F_OK);
+                test.ok(true, 'Ensuring that all modules that should be there are there');
+            }
+            catch(err)
+            {
+                console.log(err);
+            }
+        }).then(() => {
+            var ok1, ok2, ok3;
+            try
+            {
+                fs.accessSync(path.join(__dirname, 'explicitInstalls', 'prod', 'node_modules', 'nodeunit'), fs.F_OK);
+            }
+            catch(err)
+            {
+                ok1 = true;
+            }
+            try
+            {
+                fs.accessSync(path.join(__dirname, 'explicitInstalls', 'prod', 'node_modules', 'mongodb'), fs.F_OK);
+            }
+            catch(err)
+            {
+                ok2 = true;
+            }
+            try
+            {
+                fs.accessSync(path.join(__dirname, 'explicitInstalls', 'dev', 'node_modules', 'mongodb'), fs.F_OK);
+            }
+            catch(err)
+            {
+                ok3 = true;
+            }
+            test.ok(ok1 && ok2 && ok3, 'Ensuring that all modules that should not be there are not there');
+        }).then(() => {
+            /*actOnModules.sync(path.join(__dirname, 'explicitInstalls', 'prod'), (directory) => {
+                childProcess.execSync('rm -R node_modules', {'cwd': directory});
+            });
+            actOnModules.sync(path.join(__dirname, 'explicitInstalls', 'dev'), (directory) => {
+                childProcess.execSync('rm -R node_modules', {'cwd': directory});
+            });*/
         }).catch((err) => {
             console.log(err);
         }).finally(() => {
