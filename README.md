bb2fda764468dd531fd4003aa2d0c06f0bcf77e9
Eric Vallee <eric_vallee@webificservices.com>
2016-12-10 19:04:47 -0500

    Added yarn support. Changed API to make optional arguments more manageable

--- README.md
+++ README.md
@@ -4,23 +4,37 @@ The recursive installer is a tool to recursively install npm dependencies in mod
 
 #Behavior
 
-It uses 'npm install' for installing and 'act-on-modules' project to find modules (recursively traversing directories and stopping whenever it finds a package.json file).
+It uses either npm or yarn (depending on the tool option passed to the library) for installing and 'act-on-modules' project to find modules (recursively traversing directories and stopping whenever it finds a package.json file).
 
 #Usage
-
 
 ##Signature
 
 ```
 const recursiveInstaller = require('recursive-installer');
-recursiveInstaller([<Strings of paths containing modules to install npm dependencies on>], <optional argument to append to the npm install command>, <optional maxBuffer value for stdout/stderr size limits of subprocesses>) //Returns a promise
+recursiveInstaller([<Strings of paths containing modules to install npm dependencies on>], <options>) //Returns a promise
 ```
+
+```<options>``` is an object that can be passed (optionally), the following keys:
+
+- arguments: Arguments to pass to the install command (can be a string or array or strings)
+
+- maxBuffer: Maximum size allocated to stdout/stderr subprocesses (in bytes)
+
+- tool: Tool that should perform the module install. Currently, the library supports 'npm' and 'yarn'. Npm is the default if this option is omitted.
+
+optional argument to append to the npm install command 
+optional maxBuffer value for stdout/stderr size limits of subprocesses
 
 ##Example
 
 ```
 const recursiveInstaller = require('recursive-installer');
-recursiveInstaller(['/home/eric/app', '/home/eric/nodeJsModules'], '--production').then(() => {
+recursiveInstaller(['/home/eric/app', '/home/eric/nodeJsModules'], {
+    arguments: '--production',
+    maxBuffer: 5*1024*1024, //5 MB
+    tool: 'yarn'
+}).then(() => {
     console.log('all done');
 });
 ```
@@ -32,3 +46,9 @@ Run ```npm test```
 ##Installation
 
 Run ```npm install recursive-installer```
+
+##Requirements
+
+To run the yarn test or generally to run the library with the 'yarn' tool option, you need to have yarn installed.
+
+Also, the yarn part of the tests will fail if yarn is not installed.
--- test/index.js
+++ test/index.js
@@ -15,9 +15,9 @@ exports.test = {
         });
         callback();
     },
-    'basic_functionality': function(test) {
+    'basic_functionality_npm': function(test) {
         test.expect(1);
-        recursiveInstaller([path.join(__dirname, 'app'), path.join(__dirname, 'shared')], null, 1024 * 500).then(() => {
+        recursiveInstaller([path.join(__dirname, 'app'), path.join(__dirname, 'shared')], {'maxBuffer': 1024*1024*5}).then(() => {
             try
             {
                 fs.accessSync(path.join(__dirname, 'app', 'node_modules'), fs.F_OK);
@@ -30,6 +30,69 @@ exports.test = {
             {
                 console.log(err);
             }
+        }).catch((err) => {
+            console.log(err);
+        }).finally(() => {
+            test.done();
+        });
+    },
+    'basic_functionality_yarn': function(test) {
+        test.expect(2);
+        var exceptions = 0;
+        recursiveInstaller([path.join(__dirname, 'app'), path.join(__dirname, 'shared')], {'tool': 'yarn', 'maxBuffer': 1024*1024*5, 'arguments': '--no-lockfile'}).then(() => {
+            try
+            {
+                fs.accessSync(path.join(__dirname, 'app', 'node_modules'), fs.F_OK);
+                fs.accessSync(path.join(__dirname, 'shared', 'user-store', 'node_modules'), fs.F_OK);
+                fs.accessSync(path.join(__dirname, 'shared', 'array', 'capitalize-array', 'node_modules'), fs.F_OK);
+                fs.accessSync(path.join(__dirname, 'shared', 'array', 'sort-array', 'node_modules'), fs.F_OK);
+                test.ok(true, 'Ensuring that all modules got their dependencies installed properly');
+            }
+            catch(err)
+            {
+                console.log(err);
+            }
+        }).then(() => {
+            return Promise.all([
+                new Promise((resolve, reject) => {    
+                    try {
+                        fs.accessSync(path.join(__dirname, 'app', 'yarn.lock'), fs.F_OK);
+                        resolve();
+                    } catch(err) {
+                        exceptions++;
+                        resolve();
+                    }
+                }),
+                new Promise((resolve, reject) => {    
+                    try {
+                        fs.accessSync(path.join(__dirname, 'shared', 'user-store', 'yarn.lock'), fs.F_OK);
+                        resolve();
+                    } catch(err) {
+                        exceptions++;
+                        resolve();
+                    }
+                }),
+                new Promise((resolve, reject) => {    
+                    try {
+                        fs.accessSync(path.join(__dirname, 'shared', 'array', 'capitalize-array', 'yarn.lock'), fs.F_OK);
+                        resolve();
+                    } catch(err) {
+                        exceptions++;
+                        resolve();
+                    }
+                }),
+                new Promise((resolve, reject) => {    
+                    try {
+                        fs.accessSync(path.join(__dirname, 'shared', 'array', 'sort-array', 'yarn.lock'), fs.F_OK);
+                        resolve();
+                    } catch(err) {
+                        exceptions++;
+                        resolve();
+                    }
+                })
+            ]);
+        }).then(() => {
+            test.ok(exceptions === 4, "Ensure that argument to not generate lock files was properly passed")
         }).catch((err) => {
             console.log(err);
         }).finally(() => {
